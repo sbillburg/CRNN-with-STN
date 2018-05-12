fc71aee7a3b7b44e7877b0836dce4646548a9252
Junbo <abcliubill@gmail.com>
2018-05-12 15:50:24 +0800

    Update config.py

--- config.py
+++ config.py
@@ -15,8 +15,11 @@ img_folder = '/media/junbo/DATA/OCR_datasets/max/90kDICT32px'
 
 
 # for CRNN_with_STN
-learning_rate = 0.0001  # 0.002 for default
-load_model_path = '/home/junbo/PycharmProjects/test0_mnist/models/weights_best_STN.50-1.84.hdf5'
-cp_save_path = '/home/junbo/PycharmProjects/test0_mnist/models/weights_best_STN.{epoch:02d}-{loss:.2f}.hdf5'
-base_model_path = '/home/junbo/PycharmProjects/test0_mnist/models/weights_for_predict_STN.hdf5'
-tb_log_dir = '/home/junbo/PycharmProjects/test0_mnist/CRC_n/paper_log'  # Optional
+learning_rate = 0.0001  # learning rate, 0.002 for default
+cp_save_path = '/home/junbo/PycharmProjects/test0_mnist/models/weights_best_STN.{epoch:02d}-{loss:.2f}.hdf5' \
+    # save checkpoint path
+base_model_path = '/home/junbo/PycharmProjects/test0_mnist/models/weights_for_predict_STN.hdf5'  \
+    # the model for predicting
+tb_log_dir = '/home/junbo/PycharmProjects/test0_mnist/CRC_n/paper_log'  # TensorBoard save path, Optional
+load_model_path = '/home/junbo/PycharmProjects/test0_mnist/models/weights_best_STN.95-1.65.hdf5'  # Optional
+
